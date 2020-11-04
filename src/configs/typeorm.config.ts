563646e9c8f9dea56b27d51966799c156aa3a5f9
Thiago Alves <thiago.desouza.alves@hotmail.com>
2020-11-04 12:23:49 -0300

    Atualizando dados de configuracoes do database

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'pgsql',
-    port: 5432,
-    username: 'pguser',
-    password: 'pgpassword',
-    database: 'nestjs',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_HOST),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true,
+    synchronize: Boolean(process.env.DB_SYNC),
 }
